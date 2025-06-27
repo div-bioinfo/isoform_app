55d0f1a4ac2ecd21335efb7d70c394498d73848e
div-bioinfo <divya6402@gmail.com>
2025-06-27 10:51:04 +0530

    Update isoform_app.py

--- isoform_app.py
+++ isoform_app.py
@@ -754,7 +754,7 @@ with st.sidebar:
             # Only show overlap slider if reference is uploaded
             min_exon_overlap = st.slider(
                 "Minimum exon overlap ratio", 
-                min_value=0.1, max_value=1.0, value=0.7, step=0.1
+                min_value=0.1, max_value=1.0, value=0.7, step=0.1,
                 help="Filters out the rows of table with score < threshold ."
             )
     st.markdown("---")        
